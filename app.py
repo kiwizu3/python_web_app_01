31e1e653ed7f0f7a97f0e58a7a61c84ba6dcfea6
kiwi <kavindasu@gmail.com>
2024-11-19 02:38:00 +0530

    Added CORS

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app) #Enable CORS for all routes and origins
 
 DATA_FILE = "citizen_data.txt"
 
